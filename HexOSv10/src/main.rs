1a960ab4a178bee82bc2946659cc80e690634af1
DazaiZero <aniketvaidya101@gmail.com>
2022-10-25 01:20:01 +0530

    message

--- HexOSv10/src/main.rs
+++ HexOSv10/src/main.rs
@@ -3,18 +3,22 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 static HELLO: &[u8] = b"Welcome on First Boot of HexOS Decentralized Operating System ...";
 
 #[no_mangle] 
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    //let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
+    vga_buffer::print_something();
+
+    /* for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
-    }
+    } */
 
     loop {}
 }
@@ -23,3 +27,4 @@ pub extern "C" fn _start() -> ! {
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
+
